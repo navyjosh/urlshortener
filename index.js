55ccc84ba9d030b8749b6b5e523116224a639c90
Josh <code@flynimbus.cloud>
2024-03-13 19:45:51 -0400

    different match

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post("/api/shorturl", async (req, res) => {
   if (!originalUrl.match("^https?://")) {
     res.json({ error: "Invalid URL", msg: "Missing https?://" });
   } else {
-    let domain = originalUrl.match(/(\w+)\.\w{2,}(\/|\?|$)/)[1];
+    let domain = originalUrl.match(/(\w+)\.\w{2,}(\/|\?|$)/)[0];
     console.log(`domain: ${domain}`);
     console.log(`originalUrl: ${originalUrl}`);
     dns.lookup(domain, async (err, address, family) => {
